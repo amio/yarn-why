e257ee875e95f7b2904e2287e2b9eee8427a022f
Amio <amio.cn@gmail.com>
2018-08-21 13:50:30 +0800

    initial commit

---
+++ cli-main.js
@@ -0,0 +1,47 @@
+const fs = require('fs')
+const path = require('path')
+const kleur = require('kleur')
+const why = require('./index.js')
+
+/**
+ * Run npm-why in a directory
+ *
+ * @param {String} dir Workding directory
+ * @param {String} packageName The package to lookup
+ */
+module.exports = function main (dir, packageName) {
+  const reasons = why(
+    loadLockfile(dir, 'yarn.lock'),
+    packageName
+  )
+
+  if (!reasons.length) {
+    console.log(`\n  No one requires ${kleur.blue(packageName)}.`)
+  } else {
+    console.log(`\n  Who required ${kleur.blue(packageName)}:\n`)
+    print(reasons)
+    console.log('')
+  }
+}
+
+function loadLockfile (dir, file) {
+  try {
+    return fs.readFileSync(path.resolve(dir, file), 'utf8')
+  } catch (e) {
+    if (e.code === 'MODULE_NOT_FOUND') {
+      console.error(`\n  ${kleur.red('ERROR')} Cannot find ${kleur.yellow(file)}.\n`)
+    } else {
+      console.error(`\n  ${kleur.red('ERROR')} ${e.message}\n`)
+    }
+    process.exit(1)
+  }
+}
+
+function print (reasons) {
+  reasons.map(reason => {
+    const versionTag = kleur.dim('@' + reason[0].version)
+    return reason.reverse().map(rs => {
+      return kleur.blue(rs.name)
+    }).join(' > ') + versionTag
+  }).sort().forEach(x => console.log('  ' + x))
+}
---
+++ index.js
@@ -0,0 +1,66 @@
+const lockfile = require('@yarnpkg/lockfile')
+
+/**
+ * Find who depend upon a target package.
+ *
+ * @param {String} lockContent content of yarn.lock
+ * @param {String} target target package name
+ * @param {Function} printer
+ */
+module.exports = function (lockContent, name, printer) {
+  const tree = lockfile.parse(lockContent)
+
+  if (tree.type !== 'success') {
+    throw new Error('Malformed yarn.lock')
+  }
+
+  const targets = Object.keys(tree.object).filter(
+    x => parseNameVersion(x).name === name
+  )
+  console.log(targets)
+
+  // console.log(JSON.stringify(tree, null, 2))
+  // console.log(Object.keys(tree.object))
+  // console.log(targets)
+
+  // Find requiredBy for those targets
+  const results = []
+  targets.forEach(t => {
+    lookupDependents(t, tree.object, [], results)
+  })
+  return results.map(breadcrumbs => {
+    return breadcrumbs.map(b => parseNameVersion(b))
+  })
+  // return targets.reduce((accu, curr) => {
+  //   lookupDependents(curr, tree.object, [], accu)
+  //   return accu
+  // }, []).map(reasons => {
+  //
+  // })
+  // return results
+}
+
+function lookupDependents (target, root, breadcrumb = [], results) {
+  breadcrumb = breadcrumb.concat(target)
+
+  const dependents = Object.entries(root).filter(([pkg, meta]) => {
+    return meta.dependencies && Object.entries(meta.dependencies).find(x => {
+      return x.join('@') === target
+    })
+  })
+
+  if (dependents.length) {
+    dependents.forEach(dept => lookupDependents(dept[0], root, breadcrumb, results))
+  } else {
+    results.push(breadcrumb)
+  }
+}
+
+// `@yarnpkg/lockfile@^1.0.2` => { name: '@yarnpkg/lockfile', version: '^1.0.2' }
+function parseNameVersion (nameVersion) {
+  const nv = nameVersion.split('@')
+  return {
+    name: nv.slice(0, -1).join('@'),
+    version: nv.slice(-1)
+  }
+}
